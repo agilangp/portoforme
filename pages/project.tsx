986705fbfc196252564472f13f6248e7c2cae282
Alwin Gilang Permana <al.gilang.p@gmail.com>
2022-04-08 11:39:45 +0700

    center the project text

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,12 +1,12 @@
-import Layout from "../components/Layout";
+import Layout from "../components/Layout"
 import type { NextPage } from "next"
 
 const project: NextPage = () => {
     return (
         <Layout>
-            <h1>Coming Soon</h1>
+            <h1 className="text-center">Coming Soon</h1>
         </Layout>
-    );
+    )
 }
 
-export default project;
+export default project
