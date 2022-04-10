76c9d93af023706b64607527d22bc9448552eac2
Alwin Gilang Permana <al.gilang.p@gmail.com>
2022-04-10 21:28:43 +0700

    add: menu in medium or below screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { useEffect } from "react"
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react"
 import { Switch } from "@headlessui/react"
 
 interface NavbarProps {
@@ -8,17 +8,26 @@ interface NavbarProps {
 }
 
 const Navbar = ({ changeMode, darkMode }: NavbarProps) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    const dropdown = useRef<HTMLDivElement>(null)
+
     useEffect(() => {
-        if (process.browser) {
-            document
-                .getElementById("hamburger")
-                ?.addEventListener("click", () => {
-                    document
-                        .getElementById("navbar")
-                        ?.classList.toggle("hidden")
-                })
+        if (!menuIsOpen) return
+
+        function handleClick(event: MouseEvent) {
+            const target = event.target as HTMLDivElement
+            if (dropdown.current && !dropdown.current.contains(target)) {
+                setMenuIsOpen(false)
+            }
         }
-    })
+
+        window.addEventListener("click", handleClick)
+        return () => window.removeEventListener("click", handleClick)
+    }, [menuIsOpen])
+
+    const handleMenuClick = () => {
+        setMenuIsOpen(!menuIsOpen)
+    }
 
     const ShortLink = (props: any) => {
         let { href, children, ...rest } = props
@@ -26,7 +35,21 @@ const Navbar = ({ changeMode, darkMode }: NavbarProps) => {
             <Link href={href}>
                 <a
                     {...rest}
-                    className="p-3 font-semibold hover:underline hover:text-orange-500"
+                    className="p-3 mx-4 font-semibold hover:underline hover:text-orange-500"
+                >
+                    {children}
+                </a>
+            </Link>
+        )
+    }
+
+    const PopUpMenuLink = (props: any) => {
+        let { href, children, ...rest } = props
+        return (
+            <Link href={href}>
+                <a
+                    {...rest}
+                    className="inline-block p-3 font-semibold hover:underline hover:text-orange-500"
                 >
                     {children}
                 </a>
@@ -59,7 +82,7 @@ const Navbar = ({ changeMode, darkMode }: NavbarProps) => {
                         </span>
                     </a>
                 </Link>
-                <div className="md:flex hidden text-sm">
+                <div className="lg:flex hidden text-sm">
                     <ShortLink href="/project">PROJECT</ShortLink>
                     <ShortLink
                         href="https://github.com/al-gilang-p"
@@ -113,8 +136,39 @@ const Navbar = ({ changeMode, darkMode }: NavbarProps) => {
                             d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                         />
                     </svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 mx-2 cursor-pointer lg:hidden"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        onClick={handleMenuClick}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
                 </div>
             </div>{" "}
+            <hr className="dark:border-zinc-800" />
+            <div
+                className={` ${menuIsOpen ? "" : "hidden"} text-sm text-center`}
+                id="menu"
+                ref={dropdown}
+            >
+                <PopUpMenuLink href="/project">PROJECT</PopUpMenuLink>
+
+                <hr className="dark:border-zinc-800" />
+                <PopUpMenuLink
+                    href="https://github.com/al-gilang-p"
+                    target="_blank"
+                >
+                    SOURCES
+                </PopUpMenuLink>
+            </div>
             <hr className="dark:border-zinc-800" />
         </nav>
     )
