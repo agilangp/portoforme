219a12d52f09a8fdd5a9b4d2b2d46a54e0561bd7
Alwin Gilang Permana <al.gilang.p@gmail.com>
2022-04-24 10:01:56 +0700

    refactor index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,110 +1,10 @@
 import type { NextPage } from "next"
 import Image from "next/image"
-import Link from "next/link"
-import { useCallback, useState } from "react"
+import IconLink from "../components/index/IconLink"
+import ContactForm from "../components/index/ContactForm"
 import Layout from "../components/Layout"
 
 const Home: NextPage = () => {
-    const [formStatus, setFormStatus] = useState({
-        submitted: false,
-        submitting: false,
-        info: { error: false, msg: null },
-    })
-
-    const [formContent, setFormContent] = useState({
-        name: "",
-        email: "",
-        message: "",
-    })
-
-    const handleOnFormChange = useCallback(
-        (e) => {
-            const { value, name } = e.target
-            setFormContent({
-                ...formContent,
-                [name]: value,
-            })
-        },
-        [formContent, setFormContent]
-    )
-
-    const handleServerResponse = useCallback((ok, msg) => {
-        if (ok) {
-            setFormStatus({
-                submitted: true,
-                submitting: false,
-                info: { error: false, msg },
-            })
-            setFormContent({
-                name: "",
-                email: "",
-                message: "",
-            })
-        } else {
-            setFormStatus({
-                submitted: false,
-                submitting: false,
-                info: { error: true, msg },
-            })
-        }
-    }, [])
-
-    const handleOnFormSubmit = useCallback(
-        (e) => {
-            e.preventDefault()
-            setFormStatus((prevFormStatus) => ({
-                ...prevFormStatus,
-                submitting: true,
-            }))
-            fetch(
-                "https://formsubmit.co/ajax/5c0501dde9c873ce6d07be630af3a25d",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                    },
-                    body: JSON.stringify(formContent),
-                }
-            )
-                .then((response) => {
-                    handleServerResponse(true, "Your Message has been sent.")
-                })
-                .catch((error) =>
-                    handleServerResponse(false, `Error: ${error.message}`)
-                )
-        },
-        [formContent, handleServerResponse]
-    )
-
-    const resetForm = () => {
-        setFormStatus({
-            submitted: false,
-            submitting: false,
-            info: { error: false, msg: null },
-        })
-    }
-
-    const IconLink = (props: any) => {
-        let { href, children, ...rest } = props
-        return (
-            <Link href={href}>
-                <a
-                    {...rest}
-                    target="_blank"
-                    className="grow border-2 rounded-lg dark:border-orange-500 border-zinc-800 text-center hover:bg-black dark:hover:bg-orange-500 hover:text-white dark:text-orange-500 dark:hover:text-zinc-900 text-gray-800"
-                >
-                    <button
-                        className="p-2 font-semibold inline-flex"
-                        aria-label={props.name}
-                    >
-                        {children}
-                    </button>
-                </a>
-            </Link>
-        )
-    }
-
     return (
         <Layout>
             <div className="bg-white dark:bg-zinc-900 lg:grid lg:grid-cols-4 flex flex-col rounded-xl shadow-xl">
@@ -285,74 +185,7 @@ const Home: NextPage = () => {
             </div>
 
             <div className="bg-white dark:bg-zinc-900 mt-6 p-4 rounded-xl shadow-xl">
-                <p className="lg:text-left text-center mb-2 font-semibold underline text-2xl decoration-orange-500">
-                    Contact Me
-                </p>
-                {formStatus.submitting && (
-                    <p className="text-orange-500 text-center my-4 font-bold text-lg">
-                        Submitting...
-                    </p>
-                )}
-                {formStatus.info.error && (
-                    <p className="my-4 text-red-700">
-                        Error: {formStatus.info.msg}
-                    </p>
-                )}
-                {formStatus.submitted && (
-                    <p className="text-green-500 text-center my-4 font-bold text-lg">
-                        {formStatus.info.msg}
-                        <button
-                            onClick={resetForm}
-                            className="mt-2 block mx-auto w-full lg:w-auto text-sm rounded-xl p-2 text-white hover:bg-zinc-800 font-semibold dark:text-orange-500 dark:border-orange-500 dark:hover:text-zinc-800 dark:hover:bg-orange-500"
-                        >
-                            Send Another Message
-                        </button>
-                    </p>
-                )}
-                {!formStatus.submitted && !formStatus.submitting && (
-                    <form onSubmit={handleOnFormSubmit} method="post">
-                        <label htmlFor="name" className="font-semibold">
-                            Name<span className="text-red-600">*</span>
-                        </label>
-                        <input
-                            className="p-2 w-full shadow-inner rounded-xl dark:bg-zinc-800 mb-2 bg-gray-100"
-                            type="text"
-                            name="name"
-                            id="name"
-                            onChange={handleOnFormChange}
-                            autoComplete="off"
-                            required
-                        />
-                        <label htmlFor="email" className="font-semibold">
-                            Email<span className="text-red-600">*</span>
-                        </label>
-                        <input
-                            className="p-2 w-full shadow-inner rounded-xl dark:bg-zinc-800 mb-2 bg-gray-100"
-                            type="email"
-                            id="email"
-                            name="email"
-                            onChange={handleOnFormChange}
-                            autoComplete="off"
-                            required
-                        />
-                        <label htmlFor="message" className="font-semibold">
-                            Message<span className="text-red-600">*</span>
-                        </label>
-                        <textarea
-                            className="h-40 p-2 w-full shadow-inner rounded-xl dark:bg-zinc-800 mb-2 bg-gray-100"
-                            id="message"
-                            name="message"
-                            onChange={handleOnFormChange}
-                            required
-                        ></textarea>
-                        <button
-                            className="block mx-auto w-full lg:w-auto text-sm rounded-xl p-2 border-2 border-zinc-800 hover:text-white hover:bg-zinc-800 font-semibold dark:text-orange-500 dark:border-orange-500 dark:hover:text-zinc-800 dark:hover:bg-orange-500"
-                            type="submit"
-                        >
-                            Send Message
-                        </button>
-                    </form>
-                )}
+                <ContactForm />
             </div>
         </Layout>
     )
