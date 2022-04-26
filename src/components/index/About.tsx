f4e3202943383a53c8c3e39f5802ec18dd999ea5
Alwin Gilang Permana <al.gilang.p@gmail.com>
2022-04-26 22:49:12 +0700

    refactor index page

---
+++ src/components/index/About.tsx
@@ -0,0 +1,23 @@
+import Card from "./Card"
+
+const About = () => {
+    return (
+        <Card title="About">
+            <p className="text-justify">
+                <b className="text-orange-500">Gilang</b> is a man of focus,
+                commitment, and sheer f.., ohhkaaay.. I think those words is a
+                bit too much for me. Just trying to emphasize that I really love
+                and have passion for what I do. Things that keeping me up at
+                night wishing that I could do better are software development,
+                graphic design, and drawing. Currently, software development is
+                rapidly growing skill of mine which categorized me as a
+                full-stack software developer.
+            </p>
+            <p className="font-bold mt-4">
+                Based in Magetan Regency, East Java, Indonesia
+            </p>
+        </Card>
+    )
+}
+
+export default About
