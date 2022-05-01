8e60af3418fbead2fdb31833cb57a1b3c253375d
Alwin Gilang Permana <al.gilang.p@gmail.com>
2022-05-01 23:52:59 +0700

    change icon to react-icons

--- src/components/index/IconLink.tsx
+++ src/components/index/IconLink.tsx
@@ -8,14 +8,9 @@ const IconLink = (props: any) => {
             <a
                 {...rest}
                 target="_blank"
-                className="grow border-2 rounded-lg dark:border-orange-500 border-zinc-800 text-center hover:bg-black dark:hover:bg-orange-500 hover:text-white dark:text-orange-500 dark:hover:text-zinc-900 text-gray-800"
+                className="flex justify-center grow border-2 rounded-lg dark:border-orange-500 border-zinc-800 hover:bg-black dark:hover:bg-orange-500 hover:text-white dark:text-orange-500 dark:hover:text-zinc-900 text-gray-800 p-1"
             >
-                <button
-                    className="p-2 font-semibold inline-flex"
-                    aria-label={props.name}
-                >
-                    {children}
-                </button>
+                {children}
             </a>
         </Link>
     )
